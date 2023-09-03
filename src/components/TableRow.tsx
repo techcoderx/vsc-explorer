1bee7479bfb83cbe0347426cfb2a6cc2740ba15f
techcoderx <engtianxi@outlook.com>
2023-09-03 18:28:40 +0800

    payload in separate table

--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -13,9 +13,10 @@ type TableRowProps = {
   link?: string
   isInCard?: boolean
   allCardBorders?: boolean
+  minimalSpace?: boolean
 }
 
-const TableRow = ({label, value, link, isLoading, children, isInCard = false, allCardBorders = false}: TableRowProps) => (
+const TableRow = ({label, value, link, isLoading, children, isInCard = false, allCardBorders = false, minimalSpace = false}: TableRowProps) => (
   <Tr _dark={{
     borderTop: isInCard ? cardBorder : 'unset',
     borderBottom: isInCard ? cardBorder : 'unset',
@@ -27,8 +28,8 @@ const TableRow = ({label, value, link, isLoading, children, isInCard = false, al
     borderLeft: allCardBorders ? cardBorderLight : 'unset',
     borderRight: allCardBorders ? cardBorderLight : 'unset'
   }}>
-    <Td fontWeight='bold'>{label}</Td>
-    <Td>{isLoading ? <Skeleton height='20px'/> : (children ? children : (link ? <Link as={ReactRouterLink} to={link} target={!link.startsWith('/')?'_blank':'_self'}>{value}</Link> : value))}</Td>
+    <Td fontWeight='bold' padding={minimalSpace ? '10px 10px' : undefined}>{label}</Td>
+    <Td padding={minimalSpace ? '10px 10px' : undefined}>{isLoading ? <Skeleton height='20px'/> : (children ? children : (link ? <Link as={ReactRouterLink} to={link} target={!link.startsWith('/')?'_blank':'_self'}>{value}</Link> : value))}</Td>
   </Tr>
 )
 
