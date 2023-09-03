03101c1487db083efa5032449ac01e5eaf2c2099
techcoderx <git@techcoderx.com>
2023-09-03 17:54:54 +0800

    update border color for light mode

--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -3,6 +3,7 @@ import { Link as ReactRouterLink } from 'react-router-dom'
 import { ReactNode } from 'react'
 
 const cardBorder = '1px solid rgb(255,255,255,0.16)'
+const cardBorderLight = '1px solid #e2e8f0'
 
 type TableRowProps = {
   label: string
@@ -15,7 +16,17 @@ type TableRowProps = {
 }
 
 const TableRow = ({label, value, link, isLoading, children, isInCard = false, allCardBorders = false}: TableRowProps) => (
-  <Tr borderTop={isInCard ? cardBorder : 'unset'} borderBottom={isInCard ? cardBorder : 'unset'} borderLeft={allCardBorders ? cardBorder : 'unset'} borderRight={allCardBorders ? cardBorder : 'unset'}>
+  <Tr _dark={{
+    borderTop: isInCard ? cardBorder : 'unset',
+    borderBottom: isInCard ? cardBorder : 'unset',
+    borderLeft: allCardBorders ? cardBorder : 'unset',
+    borderRight: allCardBorders ? cardBorder : 'unset'
+  }} _light={{
+    borderTop: isInCard ? cardBorderLight : 'unset',
+    borderBottom: isInCard ? cardBorderLight : 'unset',
+    borderLeft: allCardBorders ? cardBorderLight : 'unset',
+    borderRight: allCardBorders ? cardBorderLight : 'unset'
+  }}>
     <Td fontWeight='bold'>{label}</Td>
     <Td>{isLoading ? <Skeleton height='20px'/> : (children ? children : (link ? <Link as={ReactRouterLink} to={link} target={!link.startsWith('/')?'_blank':'_self'}>{value}</Link> : value))}</Td>
   </Tr>
