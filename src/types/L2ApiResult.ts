4c18051c3ec433b948ec210381f90f76ac974a63
techcoderx <git@techcoderx.com>
2023-10-19 22:03:39 +0800

    L2 transaction page

--- src/types/L2ApiResult.ts
+++ src/types/L2ApiResult.ts
@@ -9,8 +9,6 @@ export type L2BlockCID = {
       txs: TransactionConfirmed[]
       timestamp: string
     }
-    payload?: string
-    signatures?: JWSSignature[]
   }
 }
 
@@ -42,4 +40,66 @@ export enum TransactionTypes {
 export type JWSSignature = {
   protected: string
   signature: string
+}
+
+export type L2TxCID = {
+  findCID: {
+    type: 'vsc-tx',
+    data: {
+      __t: 'vsc-tx'
+      __v: '0.1'
+      lock_block: string
+      tx: ContractInput | ContractOutput
+    },
+    payload: string
+    signatures: JWSSignature[]
+  }
+}
+
+// https://github.com/vsc-eco/vsc-node/blob/main/src/types/vscTransactions.ts
+export interface CoreVSCTransaction {
+  op: string
+  type: TransactionDbType
+}
+
+export interface ContractInput extends CoreVSCTransaction {
+  contract_id: string
+  op: TransactionTypes.call_contract
+  action: string
+  payload: any
+  salt?: string
+}
+
+export interface ContractUpdate extends CoreVSCTransaction {
+  op: TransactionTypes.update_contract,
+  // TBD
+}
+
+export interface ContractOutput extends CoreVSCTransaction {
+  op: TransactionTypes.contract_output,
+  contract_id: string,
+  parent_tx_id?: string,
+  inputs: Array<{
+    id: string
+  }>
+  state_merkle: string
+  //log: JsonPatchOp[]
+  //Matrix of subdocuments --> individual logs
+  log_matrix: Record<
+    string,
+    {
+      log: JsonPatchOp[]
+    }
+  >
+  chain_actions: any | null
+}
+
+export interface TransactionContractLogMatrix {
+  log: JsonPatchOp[]
+}
+
+export interface JsonPatchOp {
+  op: string
+  path: string
+  value: string | object | number
 }
