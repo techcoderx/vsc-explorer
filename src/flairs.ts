531a76f0457ac0cc4d8ab846ea30d05c096b7a08
techcoderx <engtianxi@outlook.com>
2024-05-01 12:04:34 +0800

    update multisig controller

--- src/flairs.ts
+++ src/flairs.ts
@@ -3,6 +3,7 @@ type Flairs = {
 }
 
 export const L1Accs: Flairs = {
-  'vsc.ms-8968d20c': 'Multisig Controller',
+  'vsc.ms-8968d20c': 'Multisig Controller (Old)',
+  'vsc.gateway': 'Multisig Controller',
   'vsc.network': 'Official Account'
 }
