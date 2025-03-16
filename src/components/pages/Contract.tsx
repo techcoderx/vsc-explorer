c5241e856ccbfbf17fd68976847b289ed9750eff
techcoderx <git@techcoderx.com>
2025-03-16 18:55:46 +0800

    contract source code

--- src/components/pages/Contract.tsx
+++ src/components/pages/Contract.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import {
   Text,
+  Heading,
   Box,
   Link,
   Skeleton,
@@ -19,17 +20,26 @@ import {
   Badge,
   Tooltip,
   Button,
-  Center
+  Center,
+  Flex,
+  Card,
+  CardBody,
+  Spinner,
+  Stack
 } from '@chakra-ui/react'
+import { CheckCircleIcon, QuestionIcon } from '@chakra-ui/icons'
 import { useQuery } from '@tanstack/react-query'
 import { useParams, Link as ReactRouterLink } from 'react-router'
 import { fetchCallsByContractId, fetchContractByID, fetchMembersAtBlock } from '../../requests'
 import TableRow from '../TableRow'
 import { timeAgo, getVotedMembers, thousandSeperator, abbreviateHash } from '../../helpers'
-import { l1Explorer } from '../../settings'
+import { cvApi, l1Explorer } from '../../settings'
 import { themeColorScheme } from '../../settings'
 import { ParticipatedMembers } from '../BlsAggMembers'
 import { L1ContractCallTx, L2ContractCallTx } from '../../types/HafApiResult'
+import { cvInfo, fetchSrcFiles } from '../../cvRequests'
+import { SourceFile } from '../SourceFile'
+import { CopyButton } from '../CopyButton'
 
 const callerSummary = (tx: L1ContractCallTx | L2ContractCallTx): { primary: string; add: number } => {
   return tx.input_src === 'vsc'
@@ -65,6 +75,20 @@ export const Contract = () => {
     enabled: !isError && !isLoading && !invalidContractId && !!hasStorageProof
   })
   const { votedMembers } = getVotedMembers((contract && contract.storage_proof.bv) ?? '0', members ?? [])
+  const {
+    data: verifInfo,
+    error: verifError,
+    isLoading: verifLoading
+  } = useQuery({
+    queryKey: ['vsc-cv-verif-info', contractId],
+    queryFn: async () => cvInfo(contractId!),
+    enabled: !invalidContractId
+  })
+  const { data: verifFiles } = useQuery({
+    queryKey: ['vsc-cv-files', contractId],
+    queryFn: async () => fetchSrcFiles(contractId!),
+    enabled: !invalidContractId && !!verifInfo && verifInfo.status === 'success'
+  })
   useEffect(() => {
     if (contractId)
       fetchCallsByContractId(contractId)
@@ -86,31 +110,39 @@ export const Contract = () => {
       {isLoading ? <Skeleton h={'20px'} mt={'20px'} /> : null}
       {isSuccess && !contract.error ? (
         <Box>
-          <Table>
-            <Tbody>
-              <TableRow
-                label="Creation Timestamp"
-                value={contract ? contract.created_at + ' (' + timeAgo(contract.created_at) + ')' : ''}
-                isLoading={isLoading}
-              />
-              <TableRow
-                label="Created In L1 Block"
-                value={contract.created_in_l1_block}
-                link={l1Explorer + '/b/' + contract.created_in_l1_block}
-                isLoading={isLoading}
-              />
-              <TableRow label="Created in L1 Tx" value={contract.created_in_op} link={'/tx/' + contract.created_in_op} />
-              <TableRow label="Creator" value={contract.creator} link={'/@' + contract.creator} />
-              <TableRow label="Contract Code CID" value={contract.code} />
-            </Tbody>
-          </Table>
+          <TableContainer>
+            <Table>
+              <Tbody>
+                <TableRow
+                  label="Creation Timestamp"
+                  value={contract ? contract.created_at + ' (' + timeAgo(contract.created_at) + ')' : ''}
+                  isLoading={isLoading}
+                />
+                <TableRow
+                  label="Created In L1 Block"
+                  value={contract.created_in_l1_block}
+                  link={l1Explorer + '/b/' + contract.created_in_l1_block}
+                  isLoading={isLoading}
+                />
+                <TableRow label="Created in L1 Tx" value={contract.created_in_op} link={'/tx/' + contract.created_in_op} />
+                <TableRow label="Creator" value={contract.creator} link={'/@' + contract.creator} />
+                <TableRow label="Bytecode CID">
+                  <Flex align={'center'} gap={'2'}>
+                    <Text>{contract.code}</Text>
+                    {verifInfo && verifInfo.status === 'success' ? <CheckCircleIcon color={themeColorScheme} /> : null}
+                  </Flex>
+                </TableRow>
+              </Tbody>
+            </Table>
+          </TableContainer>
           <Tabs mt={'7'} colorScheme={themeColorScheme} variant={'solid-rounded'}>
-            <TabList>
+            <TabList overflow={'scroll'} whiteSpace={'nowrap'}>
               <Tab>
                 Transactions ({txCount}
                 {!contractTxEnd ? '+' : ''})
               </Tab>
               {hasStorageProof ? <Tab>Storage Proof</Tab> : null}
+              <Tab>Source Code</Tab>
             </TabList>
             <TabPanels mt={'2'}>
               <TabPanel>
@@ -213,6 +245,86 @@ export const Contract = () => {
                   />
                 </TabPanel>
               ) : null}
+              <TabPanel>
+                {verifInfo ? (
+                  verifInfo.status === 'success' ? (
+                    <>
+                      <Flex align={'center'} gap={'2'}>
+                        <CheckCircleIcon color={themeColorScheme} />
+                        <Heading fontSize={'md'}>Contract source code verified</Heading>
+                      </Flex>
+                      <Text m={'5px 0'}>
+                        VSC Blocks has verified that the source code below matches the deployed bytecode for the contract. This
+                        does not mean the contract is safe to interact with.
+                      </Text>
+                      <Card mt={'5'} mb={'5'}>
+                        <CardBody>
+                          <TableContainer>
+                            <Table variant={'unstyled'}>
+                              <Tbody>
+                                <TableRow label="Language" value={verifInfo.lang} isInCard minimalSpace />
+                                <TableRow label="License" value={verifInfo.license} isInCard minimalSpace />
+                                <TableRow label="Verified At" isInCard minimalSpace>
+                                  {verifInfo.verified_ts + ' (' + timeAgo(verifInfo.verified_ts) + ')'}
+                                </TableRow>
+                              </Tbody>
+                            </Table>
+                          </TableContainer>
+                        </CardBody>
+                      </Card>
+                      <Stack direction="row" justifyContent="space-between" align="flex-end">
+                        <Text fontSize={'lg'}>
+                          <b>Dependencies</b>
+                          {!!verifInfo.lockfile ? (
+                            <Text display={'inline'}>
+                              {' ('}
+                              <Link href={`${cvApi}/contract/${contractId}/files/cat/${verifInfo.lockfile}`} target="_blank">
+                                View lockfile
+                              </Link>
+                              {')'}
+                            </Text>
+                          ) : (
+                            ''
+                          )}
+                        </Text>
+                        <CopyButton text={JSON.stringify(verifInfo.dependencies, null, 2)} />
+                      </Stack>
+                      <SourceFile content={JSON.stringify(verifInfo.dependencies, null, 2)} />
+                      {!!verifFiles
+                        ? verifFiles.map((f, i) => (
+                            <Box key={i} mt="3" mb="3">
+                              <Stack direction="row" justifyContent="space-between" align="flex-end">
+                                <Text fontSize={'lg'}>
+                                  <b>
+                                    File {i + 1} of {verifFiles.length}:
+                                  </b>{' '}
+                                  {f.name}
+                                </Text>
+                                <CopyButton text={f.content} />
+                              </Stack>
+                              <SourceFile content={f.content} />
+                            </Box>
+                          ))
+                        : null}
+                    </>
+                  ) : null
+                ) : verifError ? (
+                  <Text>Failed to fetch contract verification status.</Text>
+                ) : verifLoading ? (
+                  <Flex align={'center'} gap={'2'}>
+                    <Spinner size={'sm'} />
+                    <Text fontSize={'md'}>Loading contract verification status...</Text>
+                  </Flex>
+                ) : (
+                  <>
+                    <Flex align={'center'} gap={'2'}>
+                      <QuestionIcon color={themeColorScheme} />
+                      <Heading fontSize={'md'}>Contract source code not verified</Heading>
+                    </Flex>
+                    <Text m={'5px 0'}>Source code for this contract is unknown.</Text>
+                  </>
+                )}
+              </TabPanel>
             </TabPanels>
           </Tabs>
         </Box>
