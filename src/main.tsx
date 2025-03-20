fe5c642ea896d7f31362f93f63cbdc925ac660b6
techcoderx <git@techcoderx.com>
2025-03-20 14:36:38 +0800

    field validation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { extendTheme } from '@chakra-ui/react/extend-theme'
-import { Provider } from '@chakra-ui/react/provider'
+import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -29,10 +29,10 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider theme={theme}>
+    <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
-    </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 )
