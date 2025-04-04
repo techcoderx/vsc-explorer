b7767e5e2be1f10c884db68d997f65b98009619a
techcoderx <engtianxi@outlook.com>
2025-04-04 17:58:38 +0800

    tx and l1 user page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import Navbar from './components/Navbar'
 // import Home from './components/pages/Home'
 import Witnesses from './components/pages/Witnesses'
 // import Blocks from './components/pages/Blocks'
-// import NewTxs from './components/pages/NewTxs'
+import NewTxs from './components/pages/NewTxs'
 // import NewContracts from './components/pages/NewContracts'
 // import AnchorRefs from './components/pages/AnchorRefs'
 // import PageNotFound from './components/pages/404'
 // import { BlockByID, BlockByHash } from './components/pages/Block'
-// import L1User from './components/pages/L1User'
-// import L1Tx from './components/pages/L1Tx'
+import L1User from './components/pages/L1User'
+import L1Tx from './components/pages/L1Tx'
 // import L2Tx from './components/pages/L2Tx'
 import Elections from './components/pages/Elections'
 // import Epoch from './components/pages/Epoch'
@@ -93,10 +93,10 @@ const router = createBrowserRouter([
       //   path: '/block-by-hash/:blockId',
       //   element: <BlockByHash />
       // },
-      // {
-      //   path: '/transactions',
-      //   element: <NewTxs />
-      // },
+      {
+        path: '/transactions',
+        element: <NewTxs />
+      },
       // {
       //   path: '/contracts',
       //   element: <NewContracts />
@@ -117,14 +117,14 @@ const router = createBrowserRouter([
       //   path: '/anchor-ref-cid/:cid',
       //   element: <AnchorRefByHash />
       // },
-      // {
-      //   path: '/:username/:page?',
-      //   element: <L1User />
-      // },
-      // {
-      //   path: '/tx/:txid',
-      //   element: <L1Tx />
-      // },
+      {
+        path: '/:username/:page?',
+        element: <L1User />
+      },
+      {
+        path: '/tx/:txid',
+        element: <L1Tx />
+      },
       // {
       //   path: '/vsc-tx/:txid',
       //   element: <L2Tx />
