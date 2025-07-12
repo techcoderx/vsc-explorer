1f5f593aae468631f57b71eb4ab43ed85b515b13
techcoderx <git@techcoderx.com>
2025-07-12 14:51:40 +0800

    rc: show raw amount

--- src/components/pages/address/RcInfo.tsx
+++ src/components/pages/address/RcInfo.tsx
@@ -1,19 +1,24 @@
-import { Card, CardBody, CardHeader, Heading, Text } from '@chakra-ui/react'
+import { Box, Card, CardBody, CardHeader, Heading, Text } from '@chakra-ui/react'
 import { ProgressBarPct } from '../../ProgressPercent'
 import { useAddrBalance } from '../../../requests'
 
 export const AddressRcInfo = ({ addr }: { addr: string }) => {
   const { balance, isLoading } = useAddrBalance(addr)
   return (
-    <Card width={'100%'}>
-      <CardHeader marginBottom={'-15px'}>
+    <Card>
+      <CardHeader mb={'-4'}>
         <Heading size={'md'} textAlign={'center'}>
           RC Info
         </Heading>
       </CardHeader>
       <CardBody>
         {!!balance && !!balance.rc && balance.rc.max_rcs > 0 ? (
-          <ProgressBarPct val={(100 * balance.rc.amount) / balance.rc.max_rcs} fontSize="lg" height={'10px'} width={'100%'} />
+          <Box>
+            <ProgressBarPct val={(100 * balance.rc.amount) / balance.rc.max_rcs} fontSize="lg" height={'10px'} width={'100%'} />
+            <Text fontSize={'sm'} mt={'-2'}>
+              {balance.rc.amount / 1000} / {balance.rc.max_rcs / 1000}
+            </Text>
+          </Box>
         ) : isLoading ? (
           <Text>Loading balances...</Text>
         ) : (
