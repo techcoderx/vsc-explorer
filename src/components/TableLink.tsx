f59240e03075fb64649af87a38a37374d5e119f4
techcoderx <git@techcoderx.com>
2025-08-14 00:30:58 +0800

    styling: append address type icon

--- src/components/TableLink.tsx
+++ src/components/TableLink.tsx
@@ -1,20 +1,30 @@
-import { Link, Tooltip } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+import { HStack, Link, Tooltip } from '@chakra-ui/react'
 import { Link as ReactRouterLink } from 'react-router'
 import { abbreviateHash, validateHiveUsername } from '../helpers'
+import { FaEthereum, FaHive, FaFileContract } from 'react-icons/fa6'
 
-type TableLinkParams = { val: string; tooltip?: boolean; truncate?: number }
+type TableLinkParams = { val: string; tooltip?: boolean; truncate?: number; icon?: ReactNode }
 
-const TheLink = ({ val, tooltip, truncate, href }: TableLinkParams & { href: string }) => {
-  const display = abbreviateHash(val, truncate, 0)
+const iconByAddr = (addr: string) => {
+  if (addr.startsWith('hive:')) return <FaHive />
+  else if (addr.startsWith('did:pkh:eip155:1:0x')) return <FaEthereum />
+}
+
+const rmPrefix = (addr: string) => {
+  return addr.replace('hive:', '').replace('did:pkh:eip155:1:', '')
+}
+
+const TheLink = ({ val, truncate, href, icon }: TableLinkParams & { href: string }) => {
+  const display = abbreviateHash(rmPrefix(val), truncate, 0)
   return (
     <Link as={ReactRouterLink} to={href}>
-      {tooltip ? (
+      <HStack gap={'1.5'}>
+        {icon}
         <Tooltip label={val} placement={'top'}>
           {display}
         </Tooltip>
-      ) : (
-        display
-      )}
+      </HStack>
     </Link>
   )
 }
@@ -24,14 +34,14 @@ export const TxLink = ({ val, tooltip, truncate = 15 }: TableLinkParams) => {
   return <TheLink val={val} tooltip={tooltip} truncate={truncate} href={href} />
 }
 
-export const AccountLink = ({ val, tooltip, truncate = 20 }: TableLinkParams) => {
+export const AccountLink = ({ val, tooltip, truncate = 16 }: TableLinkParams) => {
   if (val.startsWith('contract:') || (val.startsWith('vsc') && validateHiveUsername(val) !== null))
     return <ContractLink val={val} tooltip={tooltip} truncate={truncate} />
   const href = `/address/${val}`
-  return <TheLink val={val} tooltip={tooltip} truncate={truncate} href={href} />
+  return <TheLink val={val} tooltip={tooltip} truncate={truncate} href={href} icon={iconByAddr(val)} />
 }
 
-export const ContractLink = ({ val, tooltip, truncate = 20 }: TableLinkParams) => {
+export const ContractLink = ({ val, tooltip, truncate = 16 }: TableLinkParams) => {
   const href = '/contract/' + val.replace('contract:', '')
-  return <TheLink val={val} tooltip={tooltip} truncate={truncate} href={href} />
+  return <TheLink val={val} tooltip={tooltip} truncate={truncate} href={href} icon={<FaFileContract />} />
 }
