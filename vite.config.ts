81a626e793ee743c78ad5ce65ee03e2527eaa475
techcoderx <git@techcoderx.com>
2025-10-31 11:58:31 +0800

    fix: polyfill buffer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['buffer'],
+      globals: {
+        Buffer: true
+      }
+    })
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
