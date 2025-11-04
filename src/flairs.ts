503fee72513abcb2a6f103a715f9df0466dcefd7
techcoderx <git@techcoderx.com>
2025-11-04 16:17:54 +0800

    flairs: update

--- src/flairs.ts
+++ src/flairs.ts
@@ -5,7 +5,8 @@ export const Flairs: {
   'hive:vsc.ms-8968d20c': 'Old Multisig Gateway',
   'hive:vsc.gateway': 'Multisig Gateway',
   'hive:vsc.network': 'Official Account',
-  'hive:vsc.dao': 'DAO Account',
+  'hive:vsc.dao': 'VSC DAO',
   'hive:techcoderx': 'Magi Blocks Dev',
+  vsc1Bem8RnoLgGPP7E2MBN52ekrdVqy2LNpSqF: 'Hello World',
   vsc1Bak1RGMgUxvLriSCoLJWZ5Ghp26ci2CiSk: 'First Go Contract'
 }
