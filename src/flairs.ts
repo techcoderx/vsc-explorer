df179755f1078f0aea4e39ffa57fe3fb208b753a
techcoderx <git@techcoderx.com>
2026-04-02 20:28:32 +0800

    flairs: dex contracts by magi team

--- src/flairs.ts
+++ src/flairs.ts
@@ -11,8 +11,16 @@ export const Flairs: {
   'hive:techcoderx': 'Magi Blocks Dev',
   vsc1Bem8RnoLgGPP7E2MBN52ekrdVqy2LNpSqF: 'Hello World',
   vsc1Bak1RGMgUxvLriSCoLJWZ5Ghp26ci2CiSk: 'First Go Contract',
+
+  // Ōkinoko
   vsc1BgfucQVHwYBHuK2yMEv4AhYua9rtQ45Uoe: 'Ōkinoko Esukurō',
   vsc1BVLuXCWC1UShtDBenWJ2B6NWpnyV2T637n: 'Ōkinoko Ināro',
   vsc1Ba9AyyUcMnYVoDVsjoJztnPFHNxQwWBPsb: 'Ōkinoko ShinDAO',
-  vsc1BiM4NC1yeGPCjmq8FC3utX8dByizjcCBk7: 'Ōkinoko Takara'
+  vsc1BiM4NC1yeGPCjmq8FC3utX8dByizjcCBk7: 'Ōkinoko Takara',
+
+  // Contracts developed and deployed by Magi team
+  vsc1BdrQ6EtbQ64rq2PkPd21x4MaLnVRcJj85d: 'Bitcoin NAM',
+  vsc1Brvi4YZHLkocYNAFd7Gf1JpsPjzNnv4i45: 'DEX Router',
+  vsc1BoaniA5HW56GuQy6pVdoZfMcVaaDfnC8kp: 'HIVE/HBD Pool',
+  vsc1BVb95YKRHAEy24XgRSaW4L6d9vB88AdwjM: 'BTC/HBD Pool'
 }
